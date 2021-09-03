51ed6905b4a9f215eadd6c7230b20fc9d3608a60
NPJayawickrama <nilangaprabhath1122@gmail.com>
2021-09-03 14:54:48 +0530

    Testing Styles Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import styled from "styled-components/"
 
+const Title = styled.h1`
+  font-size: 2em;
+  text-align:center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 function App() {
   return (
-   <h1 styel={{textAlign: "center"}}>
+  <Wrapper>
+    <Title>
      This is the Library Management Project!
-   </h1>
+   </Title>
+  </Wrapper>
+   
   );
 }
 
